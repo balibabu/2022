981da3665d5c5c2c065de628d3f9e9c3efa14345
Bali <balibabuchauhan1@gmail.com>
2022-06-23 13:28:17 +0530

    auto uploaded by bat file

---
+++ Semester_6/IDP/Programes/LAB/test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    float x=1.0/3;
+    double y=1.0/3;
+    if(x==y){
+        printf("if block\n");
+        
+    }else 
+    {
+        printf("else\n");
+        
+    }
+    
+    return 0;
+} 
