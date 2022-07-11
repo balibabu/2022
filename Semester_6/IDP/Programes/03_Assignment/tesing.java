f5ce2e4ec7465eafb5216078234dfaa74ec95e97
Bali <balibabuchauhan1@gmail.com>
2022-07-11 12:21:37 +0530

    auto uploaded by bat file

---
+++ Semester_6/IDP/Programes/03_Assignment/tesing.java
@@ -0,0 +1,11 @@
+public class StringRelated {
+    public static void main(String[] args){
+        System.out.println(canBeFormed("aa","aaa"));
+    }
+    public static boolean canBeFormed(String str1,String str2){
+        if(str1.length() != str2.length()){
+            return false;
+        }
+        return false;
+    }
+}
