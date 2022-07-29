7db7bddf8306f39a836b15294364e21aba6ece31
Bali <balibabuchauhan1@gmail.com>
2022-07-29 22:00:10 +0530

    auto uploaded by bat file

--- Semester_6/UNP/Programes/June7/client.c
+++ Semester_6/UNP/Programes/June7/client.c
@@ -8,18 +8,18 @@
 #include<arpa/inet.h>
 int main(int argc, char *argv[])
 {
-int sockfd,n,conn,len;
-char recvline[1024];
-struct sockaddr_in servaddr;
-len=sizeof(struct sockaddr_in);
-sockfd=socket(AF_INET,SOCK_DGRAM,0);
-servaddr.sin_family=AF_INET;
-servaddr.sin_addr.s_addr=inet_addr(argv[1]);//get ip from server
-servaddr.sin_port=htons(atoi(argv[2])); // Get the port from the server
-connect(sockfd,(struct sockaddr *)&servaddr,sizeof(servaddr));
-n=read(sockfd,recvline,1024);
-printf("%d\n",n);
-recvline[n]=0;
-printf("%s",recvline);
-close(sockfd);
+    int sockfd,n,conn,len;
+    char recvline[1024];
+    struct sockaddr_in servaddr;
+    len=sizeof(struct sockaddr_in);
+    sockfd=socket(AF_INET,SOCK_DGRAM,0);
+    servaddr.sin_family=AF_INET;
+    servaddr.sin_addr.s_addr=inet_addr(argv[1]);//get ip from server
+    servaddr.sin_port=htons(atoi(argv[2])); // Get the port from the server
+    connect(sockfd,(struct sockaddr *)&servaddr,sizeof(servaddr));
+    n=read(sockfd,recvline,1024);
+    printf("%d\n",n);
+    recvline[n]=0;
+    printf("%s",recvline);
+    close(sockfd);
 }
--- Semester_6/UNP/Programes/June7/server.c
+++ Semester_6/UNP/Programes/June7/server.c
@@ -8,23 +8,23 @@
 #include<time.h>
 int main(int argc, char **argv)
 {
-int listenfd,connfd,len;
-struct sockaddr_in servaddr,clientaddr;
-char buff[1024];
-time_t ticks;
-len=sizeof(struct sockaddr_in);
-listenfd=socket(AF_INET,SOCK_DGRAM,0);
-servaddr.sin_family=AF_INET;
-servaddr.sin_addr.s_addr=htonl(INADDR_ANY);
-servaddr.sin_port=htons(0);
-bind(listenfd,(struct sockaddr *)&servaddr, sizeof(servaddr));
-getsockname(listenfd, (struct sockaddr *)&servaddr, &len);
-printf("After bind ephemeral port=%d\n",(int)ntohs(servaddr.sin_port));
-listen(listenfd,5);
-connfd=accept(listenfd, (struct sockaddr *)&clientaddr,&len);
-ticks=time(NULL);
-snprintf(buff,sizeof(buff),"%s\r\n",ctime(&ticks));
-write(connfd,buff,strlen(buff));
-write(connfd,"ITER",4);
-close(connfd);
+    int listenfd,connfd,len;
+    struct sockaddr_in servaddr,clientaddr;
+    char buff[1024];
+    time_t ticks;
+    len=sizeof(struct sockaddr_in);
+    listenfd=socket(AF_INET,SOCK_DGRAM,0);
+    servaddr.sin_family=AF_INET;
+    servaddr.sin_addr.s_addr=htonl(INADDR_ANY);
+    servaddr.sin_port=htons(0);
+    bind(listenfd,(struct sockaddr *)&servaddr, sizeof(servaddr));
+    getsockname(listenfd, (struct sockaddr *)&servaddr, &len);
+    printf("After bind ephemeral port=%d\n",(int)ntohs(servaddr.sin_port));
+    listen(listenfd,5);
+    connfd=accept(listenfd, (struct sockaddr *)&clientaddr,&len);
+    ticks=time(NULL);
+    snprintf(buff,sizeof(buff),"%s\r\n",ctime(&ticks));
+    write(connfd,buff,strlen(buff));
+    write(connfd,"ITER",4);
+    close(connfd);
 }
