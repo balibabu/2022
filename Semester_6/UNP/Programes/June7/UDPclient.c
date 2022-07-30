c38a259c3eb52122605606225c718ec20a4832b7
Bali <balibabuchauhan1@gmail.com>
2022-07-30 22:00:05 +0530

    auto uploaded by bat file

--- Semester_6/UNP/Programes/June7/UDPclient.c
+++ Semester_6/UNP/Programes/June7/UDPclient.c
@@ -18,16 +18,16 @@ int main()
 	char *message = "Hello Server";
 	int sockfd, n;
 	struct sockaddr_in servaddr;
-	
+
 	// clear servaddr
 	bzero(&servaddr, sizeof(servaddr));
 	servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 	servaddr.sin_port = htons(PORT);
 	servaddr.sin_family = AF_INET;
-	
+
 	// create datagram socket
 	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-	
+
 	// connect to server
 	if(connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
 	{
@@ -39,7 +39,7 @@ int main()
 	// no need to specify server address in sendto
 	// connect stores the peers IP and port
 	sendto(sockfd, message, MAXLINE, 0, (struct sockaddr*)NULL, sizeof(servaddr));
-	
+
 	// waiting for response
 	recvfrom(sockfd, buffer, sizeof(buffer), 0, (struct sockaddr*)NULL, NULL);
 	puts(buffer);
@@ -47,4 +47,3 @@ int main()
 	// close the descriptor
 	close(sockfd);
 }
-
