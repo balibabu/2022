3ebbb8f4d07d6630d22eb46fd965f73a199702c1
Bali Babu Chauhan <balibabuchauhan1@gmail.com>
2022-09-27 21:01:48 +0530

    updating classwork contents

--- AutoUpload.bat
+++ AutoUpload.bat
@@ -3,11 +3,18 @@ ECHO ====== SOA UNIVERSITY ======
 ECHO Auto pulling, adding, commiting and pushing on Github.
 ECHO ===================================================================================
 
+ECHO ==>> Pulling
 git pull
 git status
+
+ECHO ==>> Adding
 git add .
 git status
+
+ECHO ==>> Commiting
 git commit -m"updating classwork contents"
+
+ECHO ==>> Pushing
 git push
 
 ECHO ===================================================================================
